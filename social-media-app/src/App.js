97f2b6ad071e8e424a1f89d2039ac7e47141eeb8
Hamza <jhamzaahmed@gmail.com>
2024-03-07 21:41:50 +0500

    added basic routing

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -2,9 +2,19 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
